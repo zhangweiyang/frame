4e71cf6ec109e41a293b6eb51e73058b711fe673
zhangweiyang <1521430198@qq.com>
2015-10-25 08:26:04 +0800

    修改1

--- index.php
+++ index.php
@@ -5,3 +5,33 @@
  * Date: 2015/10/24
  * Time: 23:21
  */
+Frame::run();
+
+class Frame{
+    public static function run () {
+        self::_ini_set();
+        self::_ini_const();
+        self::_ini_require();
+        load();
+    }
+
+    protected static function _ini_set () {
+        ini_set('display_errors', 1);
+        date_default_timezone_set('PRC');
+    }
+
+    protected static function _ini_const () {
+        $base_folder = '.';
+        $base = rtrim(realpath($base_folder), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $system_folder = 'system';
+        $system = rtrim(realpath($system_folder), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+
+        define('BASE_PATH', $base);
+        define('SYSTEM_PATH', $system);
+        define('CORE_PATH', $system . 'core' . DIRECTORY_SEPARATOR);
+    }
+
+    protected static function _ini_require () {
+        require_once CORE_PATH . 'loader.php';
+    }
+}
--- system/core/loader.php
+++ system/core/loader.php
@@ -6,4 +6,33 @@
  * Time: 23:39
  */
 
-echo 111;
+function load () {
+    check_uri();
+    check_permission();
+    if ($_GET['a']) {
+
+    }
+}
+
+function check_permission () {
+
+}
+
+function load_action () {
+
+}
+
+function load_view () {
+
+}
+
+function check_uri () {
+    $_GET || $_GET['v'] = 'login';
+    if (!isset($_GET['a']) && !isset($_GET['v'])) {
+        _404 ();
+    }
+}
+
+function _404 () {
+    exit('访问不存在');
+}
